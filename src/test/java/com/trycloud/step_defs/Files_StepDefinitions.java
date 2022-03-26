ce0a27e93eabf898c927b8ff77b4f8c95be637cf
mag1313 <maglovanyi13@gmail.com>
2022-03-26 19:16:12 -0400

    US09_Jack is done

--- src/test/java/com/trycloud/step_defs/Files_StepDefinitions.java
+++ src/test/java/com/trycloud/step_defs/Files_StepDefinitions.java
@@ -1,26 +1,17 @@
 package com.trycloud.step_defs;
 
-<<<<<<< HEAD
-import com.sun.source.tree.AssertTree;
-=======
->>>>>>> origin/US09_Jack
 import com.trycloud.pages.FilesPage;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-<<<<<<< HEAD
-import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 public class Files_StepDefinitions extends FilesPage {
     String expectedText;
-=======
 
-public class Files_StepDefinitions extends FilesPage {
-
->>>>>>> origin/US09_Jack
     FilesPage filesPage = new FilesPage();
 
     public void userOnTheLoginPage() {
@@ -46,30 +37,26 @@ public class Files_StepDefinitions extends FilesPage {
     @When("user write a comment inside the input box")
     public void user_write_a_comment_inside_the_input_box() {
         filesPage.commentsButton.click();
-<<<<<<< HEAD
+
         expectedText = "jack";
         filesPage.commentsBox.sendKeys(expectedText + Keys.ENTER);
 
-
         // filesPage.commentsBox.sendKeys(expectedText);
 
-=======
         filesPage.commentsBox.sendKeys("hello");
->>>>>>> origin/US09_Jack
+
     }
 
     @When("user click the submit button to post it")
     public void user_click_the_submit_button_to_post_it() {
-<<<<<<< HEAD
-       // filesPage.submitButton.click();
-=======
 
->>>>>>> origin/US09_Jack
+        // filesPage.submitButton.click();
+
     }
 
     @Then("Verify the comment is displayed in the comment section.")
     public void verify_the_comment_is_displayed_in_the_comment_section() {
-<<<<<<< HEAD
+
         // Assert.assertTrue(messageText.isDisplayed());
 
         /*System.out.println(messageText.getText());
@@ -77,11 +64,10 @@ public class Files_StepDefinitions extends FilesPage {
         Assert.assertEquals(expectedText, actualText);
 
          */
-        Driver.getDriver().findElement(By.xpath("//div[contains(text(),'"+expectedText+"')]")).isDisplayed();
+        Driver.getDriver().findElement(By.xpath("//div[contains(text(),'" + expectedText + "')]")).isDisplayed();
         System.out.println("Driver.getDriver().findElement(By.xpath(\"//div[contains(text(),'  \"+ expectedText +\" ')]\")).getText() = "
                 + Driver.getDriver().findElement(By.xpath("//div[contains(text(),'  " + expectedText + " ')]")).getText());
-=======
->>>>>>> origin/US09_Jack
+
 
     }
 
