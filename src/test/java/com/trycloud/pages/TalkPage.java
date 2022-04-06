7e7a919e27963045d7a2019fcd91cd11475a93b2
vlatkapersin <vlatka.persin@gmail.com>
2022-04-06 15:08:23 -0400

    US11 Talk Web Element in TalkPage

--- src/test/java/com/trycloud/pages/TalkPage.java
+++ src/test/java/com/trycloud/pages/TalkPage.java
@@ -1,6 +1,8 @@
 package com.trycloud.pages;
 
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class TalkPage extends LoginPage {
@@ -8,4 +10,7 @@ public class TalkPage extends LoginPage {
     public TalkPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//*[@id=\"fileList\"]/tr[2]/td[1]/a/span[1]/span")
+    public WebElement Talk;
 }
