017a685e85548017a0b2e0c617364a3ee0236585
vlatkapersin <vlatka.persin@gmail.com>
2022-04-06 13:53:36 -0400

    US11 username and password for login

--- src/test/java/com/trycloud/step_defs/US11_talk_module_step_defs.java
+++ src/test/java/com/trycloud/step_defs/US11_talk_module_step_defs.java
@@ -1,5 +1,8 @@
 package com.trycloud.step_defs;
 
+import com.trycloud.pages.LoginPage;
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,6 +11,8 @@ public class US11_talk_module_step_defs {
 
     @Given("user is on the Dashboard page")
     public void user_is_on_the_dashboard_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url2"));
+        new LoginPage().login(ConfigurationReader.getProperty("username1"),ConfigurationReader.getProperty("password"));
 
     }
     @When("the user clicks the Talk module")
