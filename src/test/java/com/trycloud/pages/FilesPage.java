49434769fcb49a07e3839563989da79474556918
cristinamartinescu <martikristi92@yahoo.com>
2022-04-06 16:40:39 -0400

    work done

--- src/test/java/com/trycloud/pages/FilesPage.java
+++ src/test/java/com/trycloud/pages/FilesPage.java
@@ -1,23 +1,44 @@
 package com.trycloud.pages;
 
-        import com.trycloud.utilities.Driver;
-        import org.openqa.selenium.WebElement;
-        import org.openqa.selenium.support.FindBy;
-        import org.openqa.selenium.support.PageFactory;
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class FilesPage {
 
+
     public FilesPage() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
+
     }
-    @FindBy(xpath = "//button[contains(.,'Settings')]")
+
+    @FindBy(xpath = "//div[@id='app-settings-header']/button")
     public WebElement settingBtn;
+
+    @FindBy(xpath = "//*[contains(label,'Show')]")
+    public List<WebElement> settinChekBoxBtn;
+
     @FindBy(xpath = "//div[@id='app-settings-content']//input[@type='checkbox']")
-    public WebElement settingOptions;
-    @FindBy(xpath = "//a[@class='icon-quota svg']/p")
-    public WebElement usedStorageParagraph;
-    @FindBy(xpath = "//span[@class='icon icon-add']")
-    public WebElement uploadBtn;
-    @FindBy(xpath = "//span[.='Upload file']")
-    public WebElement uploadFile;
+    public List<WebElement> settingCheckBoxes;
+
+    @FindBy(xpath = "//li[@id='quota']/a")
+    public WebElement beforeStorageBtn;
+
+    @FindBy(xpath = "//*[@class='icon icon-add']")
+    public WebElement uploadFileBtn;
+
+    @FindBy(xpath = "//input[@id='file_upload_start']")
+    public WebElement getUploadFileBtn;
+
+    @FindBy(id = "uploadprogressbar")
+    public WebElement uploadBar;
+
+    @FindBy(xpath = "//*[@id='quota']//a")
+    public WebElement afterStorage;
+
+
 }
+
--- src/test/java/com/trycloud/step_defs/US10.java
+++ src/test/java/com/trycloud/step_defs/US10.java
@@ -1,107 +1,92 @@
 package com.trycloud.step_defs;
 
+
 import com.trycloud.pages.FilesPage;
 import com.trycloud.pages.LoginPage;
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
-import com.trycloud.utilities.TryCloudUtility;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 public class US10 extends LoginPage {
 
     FilesPage filesPage = new FilesPage();
 
-    @Given("user enter the username and password")
-    public void userEnterTheUsernameAndPassword() {
+
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
         inputUsername.sendKeys(ConfigurationReader.getProperty("username1"));
         inputPassword.sendKeys(ConfigurationReader.getProperty("password"));
         loginBtn.click();
     }
 
-
-    @Given("user on the dashboard page")
-    public void user_on_the_dashboard_page() {
-        dashboard.click();
-
-    }
-
-
     @When("the user clicks the {string} module")
-    public void theUserClicksTheModule(String arg0) {
+    public void the_user_clicks_the_module(String page) {
         files.click();
     }
 
-    @And("user clicks Settings on the left bottom corner")
-    public void userClicksSettingsOnTheLeftBottomCorner() {
+    @When("user clicks Settings on the left bottom corner")
+    public void user_clicks_settings_on_the_left_bottom_corner() {
         filesPage.settingBtn.click();
-
     }
 
     @Then("the user should be able to click any buttons")
-    public void theUserShouldBeAbleToClickAnyButtons() {
-        filesPage.settingOptions.click();
-
-        BrowserUtils.sleep(4);
-
-        Assert.assertTrue(filesPage.settingOptions.isSelected());
-
+    public void the_user_should_be_able_to_click_any_buttons() {
+        int i = 0;
+        BrowserUtils.sleep(2);
+        Actions actions = new Actions(Driver.getDriver());
+        for (WebElement each : filesPage.settinChekBoxBtn) {
+            BrowserUtils.waitForClickablility(each, 20);
+            each.click();
+            if (!filesPage.settingCheckBoxes.get(i).isSelected()) {
+                actions.moveToElement(each).click().perform();
+                Assert.assertTrue(filesPage.settingCheckBoxes.get(i).isSelected());
+                i++;
+            }
+        }
     }
+
+    String beforeLoad, afterLoad;
 
     @And("user checks the current storage usage")
     public void userChecksTheCurrentStorageUsage() {
-
-        BrowserUtils.sleep(4);
-        Assert.assertTrue(filesPage.usedStorageParagraph.isDisplayed());
+        beforeLoad = filesPage.beforeStorageBtn.getText();
     }
 
     @And("user uploads file with the upload file option")
     public void userUploadsFileWithTheUploadFileOption() {
-
-
-        filesPage.uploadBtn.click();
-        String uploadFile = "istockphoto-862739386-1024x1024.jpeg";
-
-
-        TryCloudUtility.waitForPageToLoad(30);
-        filesPage.uploadFile.sendKeys(uploadFile);
-
-
+        String filePath = "/Users/arstanbektemirov/Desktop/AWS Acc.docx";
+        BrowserUtils.waitFor(2);
+        filesPage.uploadFileBtn.click();
+        BrowserUtils.waitForPageToLoad(10);
+        filesPage.getUploadFileBtn.sendKeys(filePath);
         try {
-            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
-
-            wait.until(ExpectedConditions.invisibilityOf(Driver.getDriver().findElement(By.xpath("//*[contains(text(),'"+uploadFile+"')]"))));
-        } catch(Exception e){
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 30);
+            wait.until(ExpectedConditions.invisibilityOf(filesPage.uploadBar));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-
-
-
     @And("user refresh the page")
     public void userRefreshThePage() {
         Driver.getDriver().navigate().refresh();
-        BrowserUtils.sleep(3);
-
     }
 
     @Then("the user should be able to see storage usage is increased")
-    public void theUserShouldBeAbleToSeeStorageUsageIsIncreased(){
-
+    public void theUserShouldBeAbleToSeeStorageUsageIsIncreased() {
+        afterLoad = filesPage.afterStorage.getText();
+        Assert.assertNotEquals(beforeLoad, afterLoad);
     }
-
-
 }
 
 
