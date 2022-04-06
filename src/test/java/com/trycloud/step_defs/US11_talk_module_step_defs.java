0749629e4fcf0dd6ce9035ea9b6c0b7ca30670db
vlatkapersin <vlatka.persin@gmail.com>
2022-04-06 13:51:08 -0400

    US11 new features and step definition file

---
+++ src/test/java/com/trycloud/step_defs/US11_talk_module_step_defs.java
@@ -0,0 +1,22 @@
+package com.trycloud.step_defs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US11_talk_module_step_defs {
+
+    @Given("user is on the Dashboard page")
+    public void user_is_on_the_dashboard_page() {
+
+    }
+    @When("the user clicks the Talk module")
+    public void the_user_clicks_the_talk_module() {
+
+    }
+    @Then("verify the title is {string}")
+    public void verify_the_title_is(String string) {
+
+    }
+
+}
